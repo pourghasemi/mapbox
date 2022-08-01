ce21e6278f583307bd988484c1fe5d1c137d445d
Shiva Pourghasemi <shiva.pourghasemi@gmail.com>
2022-08-01 09:38:35 +0430

    add

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("./src/json/features.json");
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('./src/json/features.json')
 const middlewares = jsonServer.defaults({
-  static: "./build",
-});
-const PORT = process.env.PORT || 8000;
-server.use(middlewares);
+  static: './build',
+})
+const PORT = process.env.PORT || 8000
+server.use(middlewares)
 server.use(
   jsonServer.rewriter({
-    "/api/*": "/$1",
-  })
-);
-server.use(router);
+    '/api/*': '/$1',
+  }),
+)
+server.use(router)
 server.listen(PORT, () => {
-  console.log("Server is running");
-});
-
-
+  console.log('Server is running')
+})
